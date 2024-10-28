c3bb7c914d4a24ded787c1c9af955622e7c9b527
sophia dunkley <sophia@sophia-MacBook-Air.local>
2024-10-28 17:12:22 +0200

    s24.varasto.domain Package lisätty. Tuoteclass luotu

--- src/main/java/s24/varasto/domain/Tuote.java
+++ src/main/java/s24/varasto/domain/Tuote.java
@@ -1,5 +1,81 @@
 package s24.varasto.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Tuote {
+    
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private long tuoteId;
+
+private String tyyppi;
+private String vari;
+private String koko;
+private Double hinta;
+private String valmistaja;
+
+
+public Tuote () {}
+
+public Tuote (String tyyppi,String vari,String koko,Double hinta,String valmistaja ){
+super();
+this.tyyppi=tyyppi;
+this.vari=vari;
+this.koko=koko;
+this.hinta=hinta;
+this.valmistaja=valmistaja;
+}
+
+
+public String getTyyppi() {
+    return tyyppi;
+}
+
+public void setTyyppi(String tyyppi) {
+    this.tyyppi = tyyppi;
+}
+
+public String getVari() {
+    return vari;
+}
+
+public void setVari(String vari) {
+    this.vari = vari;
+}
+
+public String getKoko() {
+    return koko;
+}
+
+public void setKoko(String koko) {
+    this.koko = koko;
+}
+
+public Double getHinta() {
+    return hinta;
+}
+
+public void setHinta(Double hinta) {
+    this.hinta = hinta;
+}
+
+public String getValmistaja() {
+    return valmistaja;
+}
+
+public void setValmistaja(String valmistaja) {
+    this.valmistaja = valmistaja;
+}
+
+@Override
+	public String toString() {
+	return "Tuote [TuoteId=" + tuoteId + ", Tyyppi=" + tyyppi + ", Väri= "+vari+
+     ", Koko="+ koko+ ", Hinta="+ hinta +", Valmistaja="+valmistaja+"]";
+	}
 
 }
