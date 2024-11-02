2de8fa1667034eacde00c2d9c1278268b6289247
Ossi <saurio.ossi@gmail.com>
2024-11-02 15:04:20 +0200

    CRUD toiminnallisuudet

--- src/main/java/s24/varasto/web/VarastoController.java
+++ src/main/java/s24/varasto/web/VarastoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import s24.varasto.domain.Tuote;
 import s24.varasto.domain.TuoteRepository;
+import s24.varasto.domain.Valmistaja;
 import s24.varasto.domain.ValmistajaRepository;
 
 @Controller
@@ -33,6 +34,21 @@ public class VarastoController {
         //localhost:8080/uusituote
 
         return "uusituote";
+    }
+
+    @GetMapping("/uusivalmistaja")
+    public String uusiValmistaja(Model model) {
+        model.addAttribute("valmistaja", new Valmistaja());
+        //localhost:8080/uusival
+
+        return "uusivalmistaja";
+    }
+
+    @PostMapping("/saveval")
+    public String saveValmistaja(Valmistaja valmistaja) {
+        vrepository.save(valmistaja);
+
+        return "redirect:tuotteet";
     }
 
     @PostMapping("/save")
