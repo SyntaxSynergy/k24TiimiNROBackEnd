9628d25aea0cafdf97344d9ff56cec388a3cb6be
Heta <hhmj28@gmail.com>
2024-11-04 18:20:45 +0200

    add JUnit5 test

--- src/test/java/s24/varasto/VarastoApplicationTests.java
+++ src/test/java/s24/varasto/VarastoApplicationTests.java
@@ -1,13 +1,21 @@
 package s24.varasto;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import s24.varasto.web.VarastoController;
 
 @SpringBootTest
 class VarastoApplicationTests {
 
+	@Autowired
+	private VarastoController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		assertThat(controller).isNotNull();
 	}
 
 }
