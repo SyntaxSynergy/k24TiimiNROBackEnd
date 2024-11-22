fb4342a9cd913240eab326396daa80b264bc3251
sophia dunkley <sophia@sophia-MacBook-Air.local>
2024-11-22 15:30:10 +0200

    cors url vaihettu localhosttin

--- src/main/java/s24/varasto/config/WebSecurityConfig.java
+++ src/main/java/s24/varasto/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig  {
 	@Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("*"); // VÄLIAIKANEN PITÄÄ MUUTTA GITHUB PAGESIN URL LOPPUVAIHEESSA. URL sallittu tekemään pyyntöjä tälle palvelimelle
+        corsConfig.addAllowedOrigin("http://localhost:5174"); // VÄLIAIKANEN PITÄÄ MUUTTA GITHUB PAGESIN URL LOPPUVAIHEESSA. URL sallittu tekemään pyyntöjä tälle palvelimelle
         corsConfig.addAllowedMethod("*"); //Antaa luvan kaikille HTTP  pyynnöille (GET, POST, etc.)
         corsConfig.addAllowedHeader("*"); // HTTP-otsikot ovar sallittuja pyynnöstä
         corsConfig.setAllowCredentials(true); // Evästeiden ja HTTP-autentikoinnin, lähettämisen pyynnöissä
