7445cc3996cef044e64413e3005b4ac096c68660
Heta <hhmj28@gmail.com>
2024-12-07 20:20:50 +0200

    added styles.css

--- src/test/java/s24/varasto/RepositoryTest.java
+++ src/test/java/s24/varasto/RepositoryTest.java
@@ -1,9 +1,5 @@
 package s24.varasto;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
